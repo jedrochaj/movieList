901d4e9f0918b3782e6e42f15a7bc57cd805faf0
jedrochaj <jakubjedrocha@gmail.com>
2026-09-10 13:42:30 +0200

    Po dodaniu formularza

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import movies from './data/movies.json';
+import moviesData from './data/movies.json';
 import MovieCard from "./components/MovieCard";
 import { useState } from 'react';
 import "./App.css";
 
 function App() {
+  const [movies, setMovies] = useState(moviesData);
   const [watched, setWatched] = useState<number[]>([]);
   const [filter, setFilter] = useState("all");
+  const [title, setTitle] = useState("");
+  const [year, setYear] = useState("");
+  const [genre, setGenre] = useState("");
+
 
   const filteredMovies = movies.filter((movie) => {
     if (filter === "watched") {
@@ -17,8 +22,42 @@ function App() {
     return true;
   });
 
+  function addMovie(e: React.FormEvent<HTMLFormElement>){
+    e.preventDefault();
+
+    if(!title.trim() || !year.trim() || !genre.trim()){
+      alert("Zadne pole nie moze byc puste");
+      return;
+    }
+    const movieYear = Number(year);
+    if(movieYear<1900 || movieYear > new Date().getFullYear()){
+      alert("Podaj poprawny rok");
+      return;
+    }
+
+    const newMovie = {
+      id: Date.now(),
+      title: title.trim(),
+      year: movieYear,
+      genre: genre.trim(),
+    }
+    setMovies([...movies, newMovie]);
+    setTitle("");
+    setYear("");
+    setGenre("");
+  }
+
   return (
     <>
+      <h2>Dodawanie filmów</h2>
+      <form onSubmit={addMovie}>
+        <input type="text" name="title" placeholder='Tytuł' value={title} onChange={(e) => setTitle(e.target.value)} /><br />
+        <input type="number" name="year" placeholder='Rok filmu' value={year} onChange={(e) => setYear(e.target.value)}/><br />
+        <input type="text" placeholder='Gatunek' name="genre" value={genre} onChange={(e) => setGenre(e.target.value)} /><br />
+        <button type="submit">Dodaj film</button>
+      </form>
+
+      <h2>Lista filmów</h2>
       <p>Obejrzane: {watched.length}/{movies.length}</p>
 
       <button onClick={() => setFilter("all")}>Wszystkie</button>
