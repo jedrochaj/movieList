1eaa8b3aafe7be95aa3df88e74f5d6be0d377eb4
jedrochaj <jakubjedrocha@gmail.com>
2026-09-11 09:04:32 +0200

    Po zmianie gatunku na tablice

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import moviesData from './data/movies.json';
+import moviesData from "./data/movies.json";
 import MovieCard from "./components/MovieCard";
-import { useState } from 'react';
+import { useState } from "react";
 import "./App.css";
 
 function App() {
@@ -9,8 +9,7 @@ function App() {
   const [filter, setFilter] = useState("all");
   const [title, setTitle] = useState("");
   const [year, setYear] = useState("");
-  const [genre, setGenre] = useState("");
-
+  const [genre, setGenre] = useState<string[]>([""]);
 
   const filteredMovies = movies.filter((movie) => {
     if (filter === "watched") {
@@ -22,15 +21,15 @@ function App() {
     return true;
   });
 
-  function addMovie(e: React.FormEvent<HTMLFormElement>){
+  function addMovie(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
-    if(!title.trim() || !year.trim() || !genre.trim()){
+    if (!title.trim() || !year.trim() || genre.some((g) => !g.trim())) {
       alert("Zadne pole nie moze byc puste");
       return;
     }
     const movieYear = Number(year);
-    if(movieYear<1900 || movieYear > new Date().getFullYear()){
+    if (movieYear < 1900 || movieYear > new Date().getFullYear()) {
       alert("Podaj poprawny rok");
       return;
     }
@@ -39,26 +38,82 @@ function App() {
       id: Date.now(),
       title: title.trim(),
       year: movieYear,
-      genre: genre.trim(),
-    }
+      genre: genre.map((g) => g.trim()),
+    };
     setMovies([...movies, newMovie]);
     setTitle("");
     setYear("");
-    setGenre("");
+    setGenre([""]);
+  }
+  function addGenre() {
+    setGenre([...genre, ""]);
+  }
+  function changeGenre(index: number, value: string) {
+    const newGenres = [...genre];
+    newGenres[index] = value;
+    setGenre(newGenres);
   }
 
   return (
     <>
       <h2>Dodawanie filmów</h2>
       <form onSubmit={addMovie}>
-        <input type="text" name="title" placeholder='Tytuł' value={title} onChange={(e) => setTitle(e.target.value)} /><br />
-        <input type="number" name="year" placeholder='Rok filmu' value={year} onChange={(e) => setYear(e.target.value)}/><br />
-        <input type="text" placeholder='Gatunek' name="genre" value={genre} onChange={(e) => setGenre(e.target.value)} /><br />
+        <input
+          type="text"
+          name="title"
+          placeholder="Tytuł"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <br />
+        <input
+          type="number"
+          name="year"
+          placeholder="Rok filmu"
+          value={year}
+          onChange={(e) => setYear(e.target.value)}
+        />
+        <br />
+        {genre.map((g, index) => (
+          <div
+            key={index}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: "5px",
+              marginBottom: "5px",
+            }}
+          >
+            <input
+              type="text"
+              placeholder="Gatunek"
+              value={g}
+              onChange={(e) => changeGenre(index, e.target.value)}
+            />
+
+            {index === genre.length - 1 && (
+              <button
+                type="button"
+                onClick={addGenre}
+                style={{
+                  backgroundColor: "#ffffff",
+                  color: "#333",
+                  border: "2px solid #666",
+                }}
+              >
+                +
+              </button>
+            )}
+          </div>
+        ))}
+
         <button type="submit">Dodaj film</button>
       </form>
 
       <h2>Lista filmów</h2>
-      <p>Obejrzane: {watched.length}/{movies.length}</p>
+      <p>
+        Obejrzane: {watched.length}/{movies.length}
+      </p>
 
       <button onClick={() => setFilter("all")}>Wszystkie</button>
       <button onClick={() => setFilter("watched")}>Obejrzane</button>
--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -2,40 +2,43 @@ import { useState } from "react";
 import "../App.css";
 
 type Props = {
-    title: string;
-    year: number;
-    genre: string;
-    isWatched: boolean;
-    onWatched: () => void;
+  title: string;
+  year: number;
+  genre: string[];
+  isWatched: boolean;
+  onWatched: () => void;
 };
 
 function MovieCard(props: Props) {
-    const [rating, setRating] = useState(0);
+  const [rating, setRating] = useState(0);
 
-    return (
-        <>
-            <div id="movie">
-                <h2>{props.title}</h2>
-                <p>Rok: {props.year}</p>
-                <p>Gatunek: {props.genre}</p>
+  return (
+    <>
+      <div id="movie">
+        <h2>{props.title}</h2>
+        <p>Rok: {props.year}</p>
+        <p>Gatunek: {props.genre.join(", ")}</p>
+        <button onClick={props.onWatched} disabled={props.isWatched}>
+          {props.isWatched ? "✓ Obejrzany" : "Oznacz jako obejrzany"}
+        </button>
+      </div>
 
-                <button onClick={props.onWatched} disabled={props.isWatched}>
-                    {props.isWatched ? "✓ Obejrzany" : "Oznacz jako obejrzany"}
-                </button>
-            </div>
-
-            <div id="rating">
-                {[1, 2, 3, 4, 5].map((stars) => {
-                    const isFilled = rating >= stars;
-                    return (
-                        <button key={stars} onClick={()=>setRating(stars)} style={{color: isFilled ? "gold" : "gray"}}>
-                            {isFilled ? "★" : "☆"}
-                        </button>
-                    );
-                })}
-            </div>
-        </>
-    );
+      <div id="rating">
+        {[1, 2, 3, 4, 5].map((stars) => {
+          const isFilled = rating >= stars;
+          return (
+            <button
+              key={stars}
+              onClick={() => setRating(stars)}
+              style={{ color: isFilled ? "gold" : "gray" }}
+            >
+              {isFilled ? "★" : "☆"}
+            </button>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default MovieCard;
